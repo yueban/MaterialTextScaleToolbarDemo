d847487a35ace6db71b10140f7aecdd45db9a269
yueban <fbzhh007@gmail.com>
2015-04-28 22:40:00 +0800

    create project MaterialTextScaleToolbarDemo

---
+++ app/src/main/java/com/bigfat/materialcontactheaderdemo/MainActivity.java
@@ -0,0 +1,150 @@
+package com.bigfat.materialcontactheaderdemo;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends ActionBarActivity implements AbsListView.OnScrollListener {
+    //控件
+    private ListView listView;
+    private Toolbar toolbar;
+    private TextView floatTitle;
+    private ImageView headerBg;
+    //测量值
+    private float headerHeight;//顶部高度
+    private float minHeaderHeight;//顶部最低高度，即Bar的高度
+    private float floatTitleLeftMargin;//header标题文字左偏移量
+    private float floatTitleSize;//header标题文字大小
+    private float floatTitleSizeLarge;//header标题文字大小（大号）
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initMeasure();
+        initView();
+        initListViewHeader();
+        initListView();
+        initEvent();
+    }
+
+    private void initMeasure() {
+        headerHeight = getResources().getDimension(R.dimen.header_height);
+        minHeaderHeight = getResources().getDimension(R.dimen.abc_action_bar_default_height_material);
+        floatTitleLeftMargin = getResources().getDimension(R.dimen.float_title_left_margin);
+        floatTitleSize = getResources().getDimension(R.dimen.float_title_size);
+        floatTitleSizeLarge = getResources().getDimension(R.dimen.float_title_size_large);
+    }
+
+    private void initView() {
+        listView = (ListView) findViewById(R.id.lv_main);
+        floatTitle = (TextView) findViewById(R.id.tv_main_title);
+        toolbar = (Toolbar) findViewById(R.id.tb_main);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initListView() {
+        List<String> data = new ArrayList<>();
+        for (int i = 0; i < 40; i++) {
+            data.add(String.valueOf(i));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.activity_list_item, android.R.id.text1, data);
+        listView.setAdapter(adapter);
+    }
+
+    private void initListViewHeader() {
+        View headerContainer = LayoutInflater.from(this).inflate(R.layout.header, listView, false);
+        headerBg = (ImageView) headerContainer.findViewById(R.id.img_header_bg);
+
+        listView.addHeaderView(headerContainer);
+    }
+
+    private void initEvent() {
+        listView.setOnScrollListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+    }
+
+    @Override
+    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+        //Y轴偏移量
+        float scrollY = getScrollY(view);
+
+        //变化率
+        float headerBarOffsetY = headerHeight - minHeaderHeight;//Toolbar与header高度的差值
+        float offset = 1 - Math.max((headerBarOffsetY - scrollY) / headerBarOffsetY, 0f);
+
+        //Toolbar背景色透明度
+        toolbar.setBackgroundColor(Color.argb((int) (offset * 255), 0, 0, 0));
+        //header背景图Y轴偏移
+        headerBg.setTranslationY(scrollY / 2);
+
+        /*** 标题文字处理 ***/
+        //标题文字缩放圆心（X轴）
+        floatTitle.setPivotX(floatTitle.getLeft() + floatTitle.getPaddingLeft());
+        //标题文字缩放比例
+        float titleScale = floatTitleSize / floatTitleSizeLarge;
+        //标题文字X轴偏移
+        floatTitle.setTranslationX(floatTitleLeftMargin * offset);
+        //标题文字Y轴偏移：（-缩放高度差 + 大文字与小文字高度差）/ 2 * 变化率 + Y轴滑动偏移
+        floatTitle.setTranslationY(
+                (-(floatTitle.getHeight() - minHeaderHeight) +//-缩放高度差
+                        floatTitle.getHeight() * (1 - titleScale))//大文字与小文字高度差
+                        / 2 * offset +
+                        (headerHeight - floatTitle.getHeight()) * (1 - offset));//Y轴滑动偏移
+        //标题文字X轴缩放
+        floatTitle.setScaleX(1 - offset * (1 - titleScale));
+        //标题文字Y轴缩放
+        floatTitle.setScaleY(1 - offset * (1 - titleScale));
+
+        //判断标题文字的显示
+        if (scrollY > headerBarOffsetY) {
+            toolbar.setTitle(getResources().getString(R.string.toolbar_title));
+            floatTitle.setVisibility(View.GONE);
+        } else {
+            toolbar.setTitle("");
+            floatTitle.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public float getScrollY(AbsListView view) {
+        View c = view.getChildAt(0);
+
+        if (c == null)
+            return 0;
+
+        int firstVisiblePosition = view.getFirstVisiblePosition();
+        int top = c.getTop();
+
+        float headerHeight = 0;
+        if (firstVisiblePosition >= 1)
+            headerHeight = this.headerHeight;
+
+        return -top + firstVisiblePosition * c.getHeight() + headerHeight;
+    }
+}
